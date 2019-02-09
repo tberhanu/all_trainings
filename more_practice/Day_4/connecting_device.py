629c890606548d07dc28893d8bb8a6d8a3f4713a
Tesfahun Berhanu <tberhanu@berkeley.edu>
2019-02-09 00:19:06 -0800

    assignment

--- more_practice/Day_4/connecting_device.py
+++ more_practice/Day_4/connecting_device.py
@@ -2,3 +2,13 @@
 Write a program that connect to a device it can be even localhost
 (use pexpect package) and print all the files in the default directory
 """
+import pexpect
+def connecting_device():
+    password = input("Enter your password: ")
+    ssh = pexpect.spawn('ssh tess@192.168.0.18 ls')
+    ssh.expect('Password:')
+    ssh.sendline(password)
+    ssh.read()
+    print(str(ssh.before, encoding="utf-8"))
+
+connecting_device()
