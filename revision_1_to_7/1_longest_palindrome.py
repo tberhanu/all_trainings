04461b2edd439dcc8d6b49d89283d11d876f86a6
Tesfahun Berhanu <tberhanu@berkeley.edu>
2019-02-10 19:25:35 -0800

    fix

--- revision_1_to_7/1_longest_palindrome.py
+++ revision_1_to_7/1_longest_palindrome.py
@@ -3,74 +3,43 @@
 """
 
 def longest_palindrome(string):
+    """
+    Runtime: 5116 ms, faster than 19.32% of Python3 online submissions for Longest Palindromic Substring.
+
+    """
     if string == "":
-            return ""
-        i = 0
-        pal = string[0]
-        while i < len(string):
-            j = len(string)
-            if len(string[i:j]) <= len(pal):
-                    return pal
-            while j >= 0:
+        return ""
+    i = 0
+    pal = string[0]
+    while i < len(string):
+        j = len(string)
+        if len(string[i:j]) <= len(pal):
+                return pal
+        while j >= 0:
 
-                if string[i:j] == string[i:j][::-1]:
-                    if len(string[i:j]) > len(pal):
-                        pal = string[i:j]
-                    break
-                j = j - 1
-            i = i + 1
-        return pal
-
-
-import numpy as np
-
-def longestPalindrome(str):
-    """
-    :type s: str
-    :rtype: str
-    """
-
-    n = len(str)
-    # pal = [][]
-    start_index = 0
-    max_length = 1
-    pal = np.zeros([n, n])
-    if str == str[::-1]:
-        return str
-    for i in range(n):
-        pal[i][i] = True
-    for i in range(n - 1):
-        if str[i] == str[i + 1]:
-            pal[i][i + 1] = True
-            start_index = i
-            max_length = 2
-        else:
-            pal[i][i + 1] = False
-    curr = 3
-    while curr <= n:
-        i = 0
-        while i < n - curr + 1:
-            j = i + curr - 1
-            if str[i] == str[j] and pal[i + 1][j - 1]:
-                pal[i][j] = True
-                if max_length < curr:
-                    max_length = curr
-                    start_index = i
-            else:
-                pal[i][j] = False
-            i = i + 1
-        curr = curr + 1
-
-    return str[start_index: start_index + max_length]
-
+            if string[i:j] == string[i:j][::-1]:
+                if len(string[i:j]) > len(pal):
+                    pal = string[i:j]
+                break
+            j = j - 1
+        i = i + 1
+    return pal
+def get_length(string):
+    if string == string[::-1]:
+        return len(string)
+    else:
+        return max(get_length(string[1:]), get_length(string[:-1]))
 
 print(longest_palindrome('eabcb'))
-
+print(get_length('eabcb'))
 print(longest_palindrome("babad"))
-print(longestPalindrome("eabcb"))
-print(longestPalindrome("babad"))
+print(get_length('babad'))
 
 print(longest_palindrome("b"))
+print(get_length("b"))
 print(longest_palindrome("baabaabk"))
+print(get_length("baabaabk"))
 print(longest_palindrome("baab"))
+print(get_length("baab"))
 print(longest_palindrome("abcdefg"))
+print(get_length("abcdefg"))
