e2a3bfd64ad5de40fcb5741af753773467c77a1c
Tesfahun Berhanu <tberhanu@berkeley.edu>
2019-02-11 18:53:52 -0800

    optimized bubble sort

--- 1_training/qn2_bubble_sort.py
+++ 1_training/qn2_bubble_sort.py
@@ -1,58 +1,5 @@
-#!/usr/bin/env python3.7
-
-import random
-import time
-
-
-def optimized_bubble_sort(arr):
-    """optimized_bubble_sort(arr) --> The optimized approach of bubble sort algorithm which takes less than
-    half the run time compared to that of the naive bubble sort algorithm.
-
-    --> Unlike the common bubble sort algorithm that keeps bubbling and swapping from  LEFT to RIGHT, here I keep
-    bubbling and swapping on both sides, to the LEFT and to the RIGHT. Even though there is run time improvement, it
-    is not good enough to sort millions of items.
-    Should I use Parallel Programming like threading or multiprocessing???
-
-
-    :param arr: Array of integers
-    :return: Sorted Array of integers
-
-    """
-
-    j = 0
-    while j + 1 < len(arr):
-        if arr[j] > arr[j + 1]:
-            swap(j, j + 1, arr)
-            left_side_sort(arr, j)
-        j = j + 1
-
-    return arr
-
-
-
-def left_side_sort(arr, j):
-    """left_side_sort(arr, j) --> Arranges the left side of the array in sorted order so that our optimized_bubble_sort
-                                function avoid the double for loop, unlike the common_bubble_sort function
-
-    :param arr: Array of integers
-    :param j: Index of the arr to the right end
-    :return:
-    """
-    while j - 1 >= 0:
-        if arr[j] < arr[j - 1]:
-            swap(j - 1, j, arr)
-            j = j - 1
-        else:
-            break
-
-def swap(i, j, arr):
-    temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
-
 def common_bubble_sort(arr):
     """common_bubble_sort(arr) --> The common approach of bubble sort algorithm optimization
-
     :param arr: Array of integers
     :return: Sorted Array of integers
     """
@@ -62,33 +9,55 @@ def common_bubble_sort(arr):
         swapped = False
         while j + 1 < len(arr):
             if arr[j] > arr[j + 1]:
-                swap(j, j + 1, arr)
+                arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
             j = j + 1
         if swapped == False:
             break
     return arr
 
+def optimized_bubble_sort(arr):
+    """
+    Here basically we keep track of the last index where we did swapping which tell us
+    everything after that index is already sorted so no need to repeat the process of
+    comparison. Therefore, every loop, we decrement our second loop range, n, depending
+    where the last swap happened.
+    """
+    n = len(arr)
+    swap = False
+    while n > 1:
+        for j in range(1, n, 1):
+            if arr[j - 1] > arr[j]:
+                arr[j - 1], arr[j] = arr[j], arr[j-1]
+                last_swap_index = j
+                swap = True
+        if swap:
+            n = last_swap_index
+            swap = False
+
+        else:
+            break
+    return arr
+
+import random
+import time
 if __name__ == "__main__":
     arr = []
-    for x in range(10000):
-      arr.append(random.randint(1, 10000))
+    for x in range(100):
+      arr.append(random.randint(1, 100))
 
 
 
 
 
-    print(optimized_bubble_sort.__doc__)
+    start_time = time.time()
+    for key, value in enumerate(common_bubble_sort(arr)):
+        print(key + 1, value)
+    print(common_bubble_sort(arr))
+    print("--- %s seconds ---" % (time.time() - start_time))
+
     start_time = time.time()
     for key, value in enumerate(optimized_bubble_sort(arr)):
         print(key + 1, value)
     print(optimized_bubble_sort(arr))
     print("--- %s seconds ---" % (time.time() - start_time))
-
-
-    # print(common_bubble_sort.__doc__)
-    # start_time = time.time()
-    # for key, value in enumerate(common_bubble_sort2(arr)):
-    #     print(key + 1, value)
-    # print(common_bubble_sort(arr))
-    # print("--- %s seconds ---" % (time.time() - start_time))
